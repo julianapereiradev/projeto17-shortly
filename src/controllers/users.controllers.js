18840415f0e5216be62eeaaf386464c39c72bdbb
juliana <julianapereira.dev@gmail.com>
2023-08-01 13:14:31 -0300

    Feat: POST /signup

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,7 +1,26 @@
 import {db} from "../database/database.connection.js"
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 export async function signup(req, res) {
-  res.send("SignUp funcionando")
+  const { name, email, password } = req.body;
+  
+  try {
+    const newEmailExistsInSignUpEmails = await db.query(`SELECT * FROM users WHERE email=$1`, [email])
+
+    if (newEmailExistsInSignUpEmails.rowCount > 0) {
+      return res.status(409).send("Este email já existe no banco");
+    }
+    
+    const encryptedPassword = bcrypt.hashSync(password, 10);
+
+    await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, encryptedPassword])
+
+    res.status(201).send("Usuário Cadastrado")
+
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
 }
 
 export async function signin(req, res) {
--- src/schemas/users.schemas.js
+++ src/schemas/users.schemas.js
@@ -3,7 +3,8 @@ import joi from "joi";
 export const signupSchema = joi.object({
     name: joi.string().trim().required(),
     email: joi.string().email().trim().required(),
-    password: joi.string().required()
+    password: joi.string().required(),
+    confirmPassword: joi.string().valid(joi.ref('password')).required(),
   });
   
   export const signinSchema = joi.object({
