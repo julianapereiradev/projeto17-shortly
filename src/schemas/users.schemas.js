58ec03bc6572ad189c6772a2eb6f90c6b10c36bc
juliana <julianapereira.dev@gmail.com>
2023-08-01 00:53:34 -0300

    Feat: Add trim in schemas

--- src/schemas/users.schemas.js
+++ src/schemas/users.schemas.js
@@ -1,12 +1,12 @@
 import joi from "joi";
 
 export const signupSchema = joi.object({
-    name: joi.string().required(),
-    email: joi.string().email().required(),
+    name: joi.string().trim().required(),
+    email: joi.string().email().trim().required(),
     password: joi.string().required()
   });
   
   export const signinSchema = joi.object({
-    email: joi.string().email().required(),
+    email: joi.string().email().trim().required(),
     password: joi.string().required()
   });
