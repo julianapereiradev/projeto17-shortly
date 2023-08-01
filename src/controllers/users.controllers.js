df02a95f0595cab4c09dd0873661277c942acade
juliana <julianapereira.dev@gmail.com>
2023-08-01 13:49:13 -0300

    Feat: POST /signin

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -9,14 +9,14 @@ export async function signup(req, res) {
     const newEmailExistsInSignUpEmails = await db.query(`SELECT * FROM users WHERE email=$1`, [email])
 
     if (newEmailExistsInSignUpEmails.rowCount > 0) {
-      return res.status(409).send("Este email já existe no banco");
+      return res.status(409).send({ messsage: "Este email já existe no banco" });
     }
     
     const encryptedPassword = bcrypt.hashSync(password, 10);
 
     await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, encryptedPassword])
 
-    res.status(201).send("Usuário Cadastrado")
+    res.status(201).send({ message: "Usuário Cadastrado" })
 
   } catch (err) {
     return res.status(500).send(err.message);
@@ -24,7 +24,33 @@ export async function signup(req, res) {
 }
 
 export async function signin(req, res) {
-  //
+
+  const { email, password } = req.body;
+  
+    try {
+
+      const userExist = await db.query(`SELECT * FROM users WHERE email=$1`, [email])
+
+      if (userExist.rowCount === 0) {
+        return res.status(401).send({ message: "Este email não existe, crie uma conta" });
+      }
+
+  
+      const hashedPassword = userExist.rows[0].password;
+
+      if (bcrypt.compareSync(password, hashedPassword)) {
+        const token = uuid();
+  
+        await db.query(`INSERT INTO sessions ("userId", token) VALUES ($1, $2)`, [userExist.rows[0].id, token])  
+        
+        res.status(200).send({ message: "Login efetuado com sucesso" })
+      
+      } else {
+        res.status(401).send({ message: "Senha incorreta!" });
+      }
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
 }
 
 export async function getUserMe(req, res) {
