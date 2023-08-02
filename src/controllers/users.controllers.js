b799ab5fc75cd33f5f03fa51ff85ded142060c51
juliana <julianapereira.dev@gmail.com>
2023-08-02 01:09:52 -0300

    Feat: GET /ranking

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,4 +1,4 @@
-import {db} from "../database/database.connection.js"
+import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
 
@@ -11,71 +11,92 @@ function mapRentalData(user_me) {
   };
 }
 
+function formatResults(results) {
+  return results.map((row) => ({
+    id: row.id,
+    name: row.name,
+    linksCount: row.linksCount,
+    visitCount: row.visitCount,
+  }));
+}
 
 export async function signup(req, res) {
   const { name, email, password } = req.body;
-  
+
   try {
-    const newEmailExistsInSignUpEmails = await db.query(`SELECT * FROM users WHERE email=$1`, [email])
+    const newEmailExistsInSignUpEmails = await db.query(
+      `SELECT * FROM users WHERE email=$1`,
+      [email]
+    );
 
     if (newEmailExistsInSignUpEmails.rowCount > 0) {
-      return res.status(409).send({ messsage: "Este email já existe no banco" });
+      return res
+        .status(409)
+        .send({ messsage: "Este email já existe no banco" });
     }
-    
+
     const encryptedPassword = bcrypt.hashSync(password, 10);
 
-    await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, encryptedPassword])
+    await db.query(
+      `INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`,
+      [name, email, encryptedPassword]
+    );
 
-    res.status(201).send({ message: "Usuário Cadastrado" })
-
+    res.status(201).send({ message: "Usuário Cadastrado" });
   } catch (err) {
     return res.status(500).send(err.message);
   }
 }
 
 export async function signin(req, res) {
-
   const { email, password } = req.body;
-  
-    try {
 
-      const userExist = await db.query(`SELECT * FROM users WHERE email=$1`, [email])
+  try {
+    const userExist = await db.query(`SELECT * FROM users WHERE email=$1`, [
+      email,
+    ]);
 
-      if (userExist.rowCount === 0) {
-        return res.status(401).send({ message: "Este email não existe, crie uma conta" });
-      }
-
-  
-      const hashedPassword = userExist.rows[0].password;
-
-      if (bcrypt.compareSync(password, hashedPassword)) {
-        const token = uuid();
-  
-        await db.query(`INSERT INTO sessions ("userId", token) VALUES ($1, $2)`, [userExist.rows[0].id, token])  
-        
-        res.status(200).send({ token: token })
-      
-      } else {
-        res.status(401).send({ message: "Senha incorreta!" });
-      }
-    } catch (err) {
-      return res.status(500).send(err.message);
+    if (userExist.rowCount === 0) {
+      return res
+        .status(401)
+        .send({ message: "Este email não existe, crie uma conta" });
     }
+
+    const hashedPassword = userExist.rows[0].password;
+
+    if (bcrypt.compareSync(password, hashedPassword)) {
+      const token = uuid();
+
+      await db.query(`INSERT INTO sessions ("userId", token) VALUES ($1, $2)`, [
+        userExist.rows[0].id,
+        token,
+      ]);
+
+      res.status(200).send({ token: token });
+    } else {
+      res.status(401).send({ message: "Senha incorreta!" });
+    }
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
 }
 
 export async function getUserMe(req, res) {
   const { authorization } = req.headers;
-  
+
   const token = authorization?.replace("Bearer ", "");
   if (!token) return res.status(401).send("Para acessar precisa de um token");
 
   try {
-    const session = await db.query(`SELECT * FROM sessions WHERE token=$1`, [token]);
+    const session = await db.query(`SELECT * FROM sessions WHERE token=$1`, [
+      token,
+    ]);
     if (session.rowCount === 0) {
       return res.status(401).send("Não foi encontrado o token no banco");
     }
-    
-    const result = await db.query(`
+
+    const result = await db.query(
+      `
     SELECT urls."userId", 
       users.name AS "name", 
       urls."id", 
@@ -91,27 +112,48 @@ export async function getUserMe(req, res) {
       urls."id", 
       urls."shortUrl", 
       urls."url"
-    ORDER BY urls."id" ASC;`, [token])
+    ORDER BY urls."id" ASC;`,
+      [token]
+    );
 
-    const total = await db.query(`
+    const total = await db.query(
+      `
     SELECT SUM(urls."visitCount") as "totalVisitCount" 
     FROM urls
     JOIN sessions ON sessions."userId" = urls."userId"
-    WHERE token=$1;`, [token]);
+    WHERE token=$1;`,
+      [token]
+    );
     const totalVisitCount = parseInt(total.rows[0].totalVisitCount, 10);
 
     res.status(200).send({
       id: result.rows[0].userId,
       name: result.rows[0].name,
       visitCount: totalVisitCount,
-      shortenedUrls: result.rows.map(mapRentalData)
-    })
-
+      shortenedUrls: result.rows.map(mapRentalData),
+    });
   } catch (err) {
     return res.status(500).send(err.message);
   }
 }
 
 export async function getRanking(req, res) {
-  //
+  const ranking = await db.query(`SELECT 
+  users.id AS "id",
+  users.name AS "nameee",
+  COUNT(urls.id) AS "linksCount",
+  COALESCE(SUM(urls."visitCount"), 0) AS "visitCount"
+FROM 
+  users
+LEFT JOIN urls ON users.id = urls."userId"
+GROUP BY 
+  users.id,
+  users.name
+ORDER BY
+  COALESCE(SUM(urls."visitCount"), 0) DESC
+LIMIT 10;
+`);
+
+  const formattedResults = formatResults(ranking.rows);
+  res.status(200).send(formattedResults);
 }
