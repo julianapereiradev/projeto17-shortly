a34e59a0cf9d789af7e208aebbba5aace6b98d79
juliana <julianapereira.dev@gmail.com>
2023-08-02 20:04:46 -0300

    Fix: Backing to last commit

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,6 +1,5 @@
 import { nanoid } from "nanoid";
 import {db} from "../database/database.connection.js"
-import { getUrlByIdDB, postUrlDB, showPostUrlDB } from "../repositories/urls.repositories.js";
 
 export async function postUrl(req, res) {
 
@@ -10,10 +9,10 @@ export async function postUrl(req, res) {
   try {
 
     const shortUrlResponse = nanoid()
-
-    postUrlDB(session.rows[0].userId, url, shortUrlResponse)
-
-    const shortUrlExist = await showPostUrlDB(url)
+    
+    await db.query(`INSERT INTO urls ("userId", url, "shortUrl") VALUES ($1, $2, $3);`, [session.rows[0].userId, url, shortUrlResponse])
+    
+    const shortUrlExist = await db.query(`SELECT * FROM urls WHERE url=$1`, [url])
 
      res.status(201).send({
       id: shortUrlExist.rows[0].id,
@@ -29,7 +28,7 @@ export async function getUrlById(req, res) {
   const { id } = req.params;
 
   try {
-    const urlIdQuery = await getUrlByIdDB(id)
+    const urlIdQuery = await db.query(`SELECT urls.id, urls."shortUrl", urls.url FROM urls WHERE id=$1`, [id]);
 
     if (urlIdQuery.rows.length === 0) {
       return res.status(404).send("Este id não existe no banco de urls");
