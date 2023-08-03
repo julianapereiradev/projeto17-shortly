5e5bb2b179c7e16dbbf932c956f0cce2ed8417f1
juliana <julianapereira.dev@gmail.com>
2023-08-03 00:07:39 -0300

    Fix: Back to the last correct commit but adding try/catch in GET /urls/open/:shortUrl

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,6 +1,6 @@
 import { nanoid } from "nanoid";
 import {db} from "../database/database.connection.js"
-import { getUrlByIdDB, postUrlDB, redirectUrlDB, selectUrlExistDB, showPostUrlDB } from "../repositories/urls.repositories.js";
+import { getUrlByIdDB, postUrlDB, showPostUrlDB } from "../repositories/urls.repositories.js";
 
 export async function postUrl(req, res) {
 
@@ -47,21 +47,21 @@ export async function redirectUrl(req, res) {
   const { shortUrl } = req.params;
 
   try {
-    const shortUrlQuery = await selectUrlExistDB(shortUrl)
+    const shortUrlQuery = await db.query(`SELECT urls.id, urls."shortUrl", urls.url, urls."visitCount" FROM urls WHERE "shortUrl"=$1`, [shortUrl]);
 
     if (shortUrlQuery.rows.length === 0) {
       return res.status(404).send("Esta url encurtada não existe no banco de urls");
     }
-
+  
     const currentVisitCount = shortUrlQuery.rows[0].visitCount;
     const newVisitCount = currentVisitCount + 1;
-
-    await redirectUrlDB(newVisitCount, shortUrl)
-
+  
+    await db.query(`UPDATE urls SET "visitCount"=$1 WHERE "shortUrl"=$2`, [newVisitCount, shortUrl]);
+  
     res.redirect(shortUrlQuery.rows[0].url);
   } catch (err) {
     return res.status(500).send(err.message);
-  }  
+  }
 }
 
 
--- src/repositories/urls.repositories.js
+++ src/repositories/urls.repositories.js
@@ -10,12 +10,4 @@ export async function showPostUrlDB(url) {
 
 export async function getUrlByIdDB(id) {
     return await db.query(`SELECT urls.id, urls."shortUrl", urls.url FROM urls WHERE id=$1`, [id]);
-}
-
-export async function selectUrlExistDB(shortUrl) {
-    return await db.query(`SELECT urls.id, urls."shortUrl", urls.url, urls."visitCount" FROM urls WHERE "shortUrl"=$1`, [shortUrl]);
-}
-
-export async function redirectUrlDB(newVisitCount, shortUrl) {
-    return await db.query(`UPDATE urls SET "visitCount"=$1 WHERE "shortUrl"=$2`, [newVisitCount, shortUrl]);
-}
+ }
